680fb60783c705964d804ae3c6133653a524f1ca
kimu3woo <junjjun@naver.com>
2020-08-09 14:15:05 +0900

    :sparkles: Add first example

---
+++ Example1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("값을 입력해주세요: ");
+            string str = Console.ReadLine();
+            float a;
+            a = float.Parse(str);
+            Console.Write("값을 입력해주세요: ");
+            str = Console.ReadLine();
+            float b;
+            b = float.Parse(str);
+            float result = a * b;
+            Console.WriteLine((int)result);
+        }
+    }
+}
